18e3e29bb48971e6b77b70fbd8c95294aaa9e557
Yevhenii Kyrmyzy <yevhenii_kyrmyzy@icloud.com>
2023-04-12 01:02:11 +0300

    feat: update methods

--- KP2/main.py
+++ KP2/main.py
@@ -1,5 +1,5 @@
 import math
-from typing import Tuple
+from typing import Callable, Tuple
 
 
 def function_f(x):
@@ -18,51 +18,58 @@ def function_q(x):
     return (x ** 3) + (0.4 * x ** 2) + (0.6 * x) - 1.6
 
 
-def bisection(func: callable, x1: float, x2: float, eps: float, n: int) -> Tuple[float, float]:
+def bisection(func: Callable[[float], float], x1: float, x2: float, eps: float, n: int) -> Tuple[float, float]:
     mn = 0.5 * (x1 + x2)
     f_x1 = func(x1)
     f_mn = func(mn)
     n += 1
-    if math.fabs(x2 - x1) > eps:
+
+    if abs(x2 - x1) > eps:
         tmp = f_x1 * f_mn
         if tmp <= 0:
             return bisection(func, x1, mn, eps, n)
         else:
             return bisection(func, mn, x2, eps, n)
     else:
-        if math.fabs(f_mn) > eps:
-            print("This function doesn't have an answer in this range or the algorithm can't find it.")
+        if abs(f_mn) > eps:
+            print("This function doesn`t have an answer in this range or the algorithm can`t find it.")
             return -1, -1
         else:
             return mn, (x2 - x1) / pow(2, n + 1)
 
 
-def chord_method(func: callable, x1: float, x2: float, eps: float, n: int) -> Tuple[float, float]:
-    while abs(x2 - x1) > 2 * eps:
+def chord_method(func: Callable[[float], float], x1: float, x2: float, eps: float, n: int) -> Tuple[float, float]:
+
+    while True:
         x1 = x2 - func(x2) * (x2 - x1) / (func(x2) - func(x1))
         x2 = x1 - func(x1) * (x1 - x2) / (func(x1) - func(x2))
         n += 1
 
+        if abs(x2 - x1) <= 2 * eps:
+            break
+
     return x2, (x2 - x1) / pow(2, n + 1)
 
 
-def secant_method(func: callable, x1: float, x2: float, eps: float, n: int) -> Tuple[float, float]:
-    res, tmp = 0, 0
+def secant_method(func: Callable[[float], float], x1: float, x2: float, eps: float, n: int) -> Tuple[float, float]:
+    res = 0
     x1 = x2 + 0.1
-    while abs(tmp - res) > 2 * eps:
+
+    while True:
         tmp = res
         res = x2 - func(x2) * (x2 - x1) / (func(x2) - func(x1))
-        x1, x2 = x2, res
+        x1 = x2
+        x2 = res
         n += 1
+
+        if abs(tmp - res) <= 2 * eps:
+            break
 
     return x2, (x2 - x1) / pow(2, n + 1)
 
 
-def newton(func: callable, x1: float, x2: float, eps: float, n: int) -> Tuple[float, float]:
+def newton(func: Callable[[float], float], x1: float, x2: float, eps: float, n: int) -> Tuple[float, float]:
     x = x2
-    f_x = 0
-    f_xa = 0
-    df = 0
     alpha = 1e-9
     delta = 1
 
@@ -73,16 +80,16 @@ def newton(func: callable, x1: float, x2: float, eps: float, n: int) -> Tuple[fl
         delta = f_x / df
         x -= delta
         n += 1
+
         if x <= x1:
             break
+
     return x, (x2 - x1) / pow(2, n + 1)
 
 
-def newton_simple(func: callable, x1: float, x2: float, eps: float, n: int) -> Tuple[float, float]:
+
+def newton_simpl(func: Callable[[float], float], x1: float, x2: float, eps: float, n: int) -> Tuple[float, float]:
     x = x2
-    f_x = 0
-    f_xa = 0
-    df = 0
     alpha = 1e-9
     delta = 1
 
@@ -95,13 +102,14 @@ def newton_simple(func: callable, x1: float, x2: float, eps: float, n: int) -> T
         delta = f_x / df
         x -= delta
         n += 1
+
         if x <= x1:
             break
 
     return x, (x2 - x1) / pow(2, n + 1)
 
 
-def simple_iteration(func: callable, x1: float, x2: float, eps: float, n: int) -> Tuple[float, float]:
+def simple_iteration(func: Callable[[float], float], x1: float, x2: float, eps: float, n: int) -> Tuple[float, float]:
     x1 = -2
     b = 0
     y = 0
@@ -111,6 +119,7 @@ def simple_iteration(func: callable, x1: float, x2: float, eps: float, n: int) -
         b = abs(x1 - y)
         x1 = y
         n += 1
+
         if b < eps:
             break
 
